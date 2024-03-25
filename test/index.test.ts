efffd6b4d525068d8e7e791d3f82accf52672e84
Chang <areschang114@qq.com>
2024-03-25 16:57:08 +0800

    chore: 补充 useH1Title 测试用例

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,16 +2,63 @@ import { describe, expect, it } from 'vitest'
 import { generateSidebar } from '../src'
 
 describe('生成侧边栏', () => {
-  it('返回侧边栏组', () => {
-    const paths = [
-      'web/index.md',
-      'web/js.md',
-      'web/css/index.md',
-      'web/css/background.md',
-      'linux/index.md',
-      'linux/wsl.md',
-    ]
+  const paths = [
+    'web/index.md',
+    'web/js.md',
+    'web/css/index.md',
+    'web/css/background.md',
+    'linux/index.md',
+    'linux/wsl.md',
+  ]
+
+  it('生成默认配置', () => {
     expect(generateSidebar('./playground', paths, {})).toMatchInlineSnapshot(`
+      {
+        "/linux/": [
+          {
+            "items": [
+              {
+                "link": "/linux/index",
+                "text": "Linux",
+              },
+              {
+                "link": "/linux/wsl",
+                "text": "WSL",
+              },
+            ],
+            "text": "linux",
+          },
+        ],
+        "/web/": [
+          {
+            "items": [
+              {
+                "link": "/web/index",
+                "text": "Web Title",
+              },
+              {
+                "link": "/web/js",
+                "text": "js",
+              },
+              {
+                "items": [
+                  {
+                    "link": "/web/css/background",
+                    "text": "background",
+                  },
+                ],
+                "text": "css",
+              },
+            ],
+            "text": "web",
+          },
+        ],
+      }
+    `)
+  })
+
+  it('使用文件 H1 标题', () => {
+    expect(generateSidebar('./playground', paths, { useH1Title: true })).toMatchInlineSnapshot(`
       {
         "/linux/": [
           {
